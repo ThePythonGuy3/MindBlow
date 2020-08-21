c2d772539f753212de48fe0f94c71aab63469359
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-21 16:43:32 +0200

    bruh

--- scripts/changer.js
+++ scripts/changer.js
@@ -17,7 +17,7 @@ changerBullet.smokeEffect = Fx.none;
 changerBullet.shootEffect = Fx.none;
 
 const explosioneffectforthis = newEffect(20, e => {
-    Draw.color("aaffaa")
+    Draw.color(Color.valueOf("aaffaa"));
     Lines.stroke(e.fin()*5);
     Lines.circle(e.x, e.y, e.fin() * 60);
     Lines.stroke(1);

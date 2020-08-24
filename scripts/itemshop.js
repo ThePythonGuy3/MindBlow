1fca65f3d60c3ad0451f27788e70e7f93a81a836
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-24 17:03:48 +0200

    we know

--- scripts/itemshop.js
+++ scripts/itemshop.js
@@ -109,7 +109,7 @@ const itemshop = extendContent(Block, "itemshop", {
 							//nothing
 						})).growX();
 						tb.row();*/
-                        //h
+                        //hhh
 					};
 				})).growX()/*.width(Core.graphics.width/3).height(Core.graphics.height*0.8)*/;
 				if(Vars.mobile){
